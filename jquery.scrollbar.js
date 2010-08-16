12eff9ee3915fef9d0ffc64570aed388212db321
Thomas Duerr <thomduerr@gmail.com>
2010-08-17 01:20:25 +0200

    refactored code to use stateful objects from a class

--- jquery.scrollbar.js
+++ jquery.scrollbar.js
@@ -29,12 +29,9 @@
         defaults: {
             handleMinHeight: 30,         // min-height of handle (height is actually dependent on content height) 
             option2: "something"
-        },
-
-        makeSomething: function(){
-//            console.log("makeSomething: ", $.scrollbar.defaults.option1);
         }
     };
+
 
 
     $.fn.scrollbar = function(options){
@@ -42,7 +39,117 @@
         // Extend default options
         var options = $.extend({}, $.scrollbar.defaults, options);
 
+        //
+        // Scrollbar class properties
+        //
+        var Scrollbar = function(container, props){
+            this.container = container || null;
+            this.props =     props || {};
+            this.mouse =     {};
+        };
+        
+        //
+        // Scrollbar class methods
+        //
+        Scrollbar.prototype = {
+            
+            //
+            // build DOM nodes for pane and scroll-handle
+            //
+            //      <div class="scrollbar">
+            //          <div class="scrollbar-pane">
+            //              [...]
+            //          </div>
+            //          <div class="scrollbar-handle-container">
+            //              <div class="scrollbar-handle"></div>
+            //          </div>
+            //          <div class="scrollbar-handle-up"></div>
+            //          <div class="scrollbar-handle-down"></div>
+            //      </div>
+            //
+            buildHtml: function(){
+                var cont = this.container;
+                cont.children().wrapAll('<div class="scrollbar-pane" />');
+                cont.append('<div class="scrollbar-handle-container"><div class="scrollbar-handle" /></div>')
+                    .append('<div class="scrollbar-handle-up" />')
+                    .append('<div class="scrollbar-handle-down" />');
 
+                cont.handle = cont.find('.scrollbar-handle');
+                cont.handleContainer = cont.find('.scrollbar-handle-container');
+            },
+            
+            //
+            // calculate height of handle.
+            // height of handle should indicate height of content.
+            //
+            setHandleHeight: function(){
+                var cont = this.container;
+                var prop = this.props;
+                prop.handleContainerHeight = cont.handleContainer.height();
+                prop.handleHeight = Math.max(prop.containerHeight * prop.handleContainerHeight / prop.contentHeight, options.handleMinHeight);
+                cont.handle.height(prop.handleHeight);
+            },
+            
+            //
+            // append events on handle
+            //
+            appendEvents: function(){
+                var cont = this.container;
+                cont.handle.bind('mousedown.handle', {elem: this}, startHandleMove);
+            },
+            
+            
+            
+        };
+        
+        //
+        // get mouse position
+        //
+        var getMousePos = function (event) {
+			return event['pageY'] || (event['clientY'] + (document.documentElement['scrollTop'] || document.body['scrollTop'])) || 0;
+		};
+		
+        //
+        // start moving of handle
+        //
+        var startHandleMove = function(ev){
+            ev.preventDefault();
+            var self = $(ev.data.elem)[0];
+            self.container.handle.top = self.container.handle.offset().top;
+            self.mouse.start = getMousePos(ev);
+            self.container.handle.addClass('move');
+    		$(document).bind('mousemove.handle', {elem: self}, onHandleMove).bind('mouseup.handle', {elem: self}, endHandleMove);
+
+console.log('startHandleMove', self.container.handle.top);
+        };
+
+        //
+        // on moving of handle
+        //
+        var onHandleMove = function(ev){
+            var self = $(ev.data.elem)[0];
+            self.mouse.delta = getMousePos(ev) - self.mouse.start;
+            self.mouse.top = self.mouse.delta + self.container.handle.top;
+console.log('onHandleMove', self.mouse.delta, self.mouse.top, self.container.handle.top);
+            self.container.handle.css({'top': self.mouse.delta + 'px'});
+        };
+
+
+        //
+        // end moving of handle
+        //
+        var endHandleMove = function(ev){
+            var self = $(ev.data.elem)[0];
+            self.container.handle.top = self.container.handle.offset().top;
+    		$(document).unbind('mousemove.handle', onHandleMove).unbind('mouseup.handle', endHandleMove);
+            self.container.handle.removeClass('move');
+        };
+        
+		
+        
+        
+
+/*
         // Properties
         var props = {};
         
@@ -149,6 +256,10 @@
     		$(document).unbind('mousemove.handle', onHandleMove).unbind('mouseup.handle', endHandleMove);
             container.handle.removeClass('move');
         };
+*/
+
+        
+
 
 
         //
@@ -157,7 +268,7 @@
         return this.each(function(i){
 
             var container = $(this);
-            
+            var props = {};
             
             // determine heights
             props.containerHeight = container.height();
@@ -170,7 +281,10 @@
 
             // append scrollbar only if neccessary
             if(props.contentHeight > props.containerHeight){
-                append.scrollbar(i).to(container);
+                var scrollbar = new Scrollbar(container, props);
+                scrollbar.buildHtml();
+                scrollbar.setHandleHeight();
+                scrollbar.appendEvents();
             }
         });
     }
@@ -183,7 +297,7 @@
     * Function that is not accessible from the outside
     */
     function someFunction1(){
-        console.log("someFunction1: ",  $.scrollbar.defaults.option2);
+//        console.log("someFunction1: ",  $.scrollbar.defaults.option2);
     }
 
 })(jQuery);  // inject global jQuery object
