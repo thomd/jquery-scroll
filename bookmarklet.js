7dbbe0bfaab83316384d4f00fb91829e669e8f2e
Thomas Duerr <thomduerr@gmail.com>
2010-10-04 00:34:06 +0200

    renamed plugin

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,4 +1,4 @@
-// javascript:(function(){var%20b=document.createElement(%22link%22);b.type=%22text/css%22;b.rel=%22stylesheet%22;b.href=%22http://localhost.jquery/jquery-scrollbar/css/scrollbar.css%22;document.getElementsByTagName(%22head%22)[0].appendChild(b);var%20a=document.createElement(%22script%22);a.type=%22text/javascript%22;a.src=%22http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js%22;a.onload=function(){var%20c=document.createElement(%22script%22);c.type=%22text/javascript%22;c.src=%22http://localhost.jquery/jquery-scrollbar/jquery.scrollbar.js%22;document.getElementsByTagName(%22body%22)[0].appendChild(c)};document.getElementsByTagName(%22body%22)[0].appendChild(a)})();
+// javascript:(function(){var%20b=document.createElement(%22link%22);b.type=%22text/css%22;b.rel=%22stylesheet%22;b.href=%22http://localhost.jquery/jquery-scrollbar/css/scrollbar.css%22;document.getElementsByTagName(%22head%22)[0].appendChild(b);var%20a=document.createElement(%22script%22);a.type=%22text/javascript%22;a.src=%22http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js%22;a.onload=function(){var%20c=document.createElement(%22script%22);c.type=%22text/javascript%22;c.src=%22http://localhost.jquery/jquery-scrollbar/jquery.scroll.js%22;document.getElementsByTagName(%22body%22)[0].appendChild(c)};document.getElementsByTagName(%22body%22)[0].appendChild(a)})();
 
 (function(){
     var style = document.createElement("link");
@@ -13,7 +13,7 @@
     script.onload = function(){
         var script = document.createElement('script');
         script.type = "text/javascript";
-        script.src = "http://localhost.jquery/jquery-scrollbar/jquery.scrollbar.js";
+        script.src = "http://localhost.jquery/jquery-scrollbar/jquery.scroll.js";
         document.getElementsByTagName('body')[0].appendChild(script);
     };
     document.getElementsByTagName('body')[0].appendChild(script);
