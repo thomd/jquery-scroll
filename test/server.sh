0e67a77e15786f2a703b7d2ed0924f49148dd3dd
Thomas Duerr <thomduerr@gmail.com>
2011-07-11 23:38:45 +0200

    added a script for starting test server and for running tests

---
+++ test/server.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -jar test/lib/JsTestDriver-1.2.2.jar --port 8448 --browser test/lib/firefox.sh --config test/scroll.conf
---
+++ test/test.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -jar test/lib/JsTestDriver-1.2.2.jar --config test/scroll.conf --tests all
