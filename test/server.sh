54d558babb733032e220bdc388f66f4135ff2b1d
Thomas Duerr <thomduerr@gmail.com>
2011-07-12 01:23:34 +0200

    run test server with more browsers

--- test/server.sh
+++ test/server.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-java -jar test/lib/JsTestDriver-1.2.2.jar --port 8448 --browser test/lib/firefox.sh --config test/scroll.conf
+# java -jar test/lib/JsTestDriver-1.2.2.jar --port 8448 --browser test/lib/firefox.sh --config test/scroll.conf
+java -jar test/lib/JsTestDriver-1.2.2.jar --port 8448 --browser test/lib/firefox.sh,test/lib/safari.sh,test/lib/chrome.sh --config test/scroll.conf
